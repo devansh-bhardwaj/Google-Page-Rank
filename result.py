ebfa4ad479d60d838912a953c7637d9e3b5f0e48
Devansh Bhardwaj <95136306+devansh-bhardwaj@users.noreply.github.com>
2023-10-23 07:14:30 +0530

    Update result.py

--- result.py
+++ result.py
@@ -9,16 +9,14 @@ def extract_num(filename):
 	return int(filename[4:-4])
 
 def plot_difference():
-
 	differences = []
-
+	
 	for filename in sorted(os.listdir(difference_path), key = extract_num):
 		with open(difference_path + '/' + filename, "r") as file:
 			difference = file.read().strip()
 			differences.append(float(difference))
 
 	iterations = len(differences)+1
-
 
 	plt.plot(range(2, iterations + 1), differences, marker = 'o', linestyle = '-')
 	plt.axhline(y = threshold, color = 'r', linestyle = 'dashed', label = "threshold")
@@ -28,15 +26,11 @@ def plot_difference():
 	plt.ylabel("Difference")
 	plt.title("Converging Difference")
 	plt.legend()
+	
 	plt.savefig("results/convergence.png")	
 	plt.close()
 
 
 if __name__ == '__main__':
-
     threshold = float(sys.argv[1])
     plot_difference()
-   
-
-
-
